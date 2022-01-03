f0a37b76b543376ddcd134bb157992e19b70cb53
Rachel Logan <dec11rll@gmail.com>
2022-01-03 12:44:48 -0500

    table instead of p tags

--- file-reader.js
+++ file-reader.js
@@ -5,13 +5,16 @@ fetch('sandwiches-menu.csv')
 	.then(text => {
 		var sandwichList = text.split('\n')
 	
-		var result
+		var result = "<table>"
 		for (var i = 1; i<sandwichList.length-1;i += 1){
 			var sandwich = sandwichList[i].split(',')
-			result += `<strong>${sandwich[0]}:</strong>
-			 ${sandwich[1]}/${sandwich[2]}<br>`
+			result += `<tr>
+							<td><strong>${sandwich[0]}:</strong></td>
+							<td>${sandwich[1]}</td>
+							<td>${sandwich[2]}</td>
+						</tr>`
 		}
-
+		result += "</table>"
 		tableDOM.innerHTML = result
 
 	})
